6c4345d34f99367fec19225cadc1da1eaef7eeca
VV <116268809+vigneshwarvj@users.noreply.github.com>
2025-02-03 19:19:04 +0530

    Update TradingViewChart.jsx
    Making the chart as dark mode

--- src/Components/TradingViewChart.jsx
+++ src/Components/TradingViewChart.jsx
@@ -6,7 +6,7 @@ const TradingViewChart = () => {
         id="dextools-widget"
         title="DEXTools Trading Chart"
         className="w-full h-64 sm:h-80 md:h-96 lg:h-[400px] xl:h-[500px]"
-        src="https://www.dextools.io/widget-chart/en/bnb/pe-light/0x23dde40ac13927ed70f8c8caefe8c2653a5d001b?theme=light&chartType=2&chartResolution=30&drawingToolbars=false"
+        src="https://www.dextools.io/widget-chart/en/bnb/pe-light/0x23dde40ac13927ed70f8c8caefe8c2653a5d001b?theme=dark&chartType=2&chartResolution=30&drawingToolbars=false"
         frameBorder="0"
         allowFullScreen
         style={{ border: 'none' }}

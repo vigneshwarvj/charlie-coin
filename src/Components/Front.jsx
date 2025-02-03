572206659266cb9fed59c53353b621469343b592
VV <116268809+vigneshwarvj@users.noreply.github.com>
2025-02-03 18:56:55 +0530

    Update Front.jsx
    Updated logo in front

--- src/Components/Front.jsx
+++ src/Components/Front.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import the AOS styles
+import logo from '../assets/images/Logo.png';
 
 const Front = () => {
 
@@ -24,7 +25,7 @@ const Front = () => {
         />
 
         <img
-          src="https://cdn.discordapp.com/attachments/1317487496170307636/1333417299050364948/Logo_1.png?ex=679acb61&is=679979e1&hm=7b49e17b1bd7905fbb4ba593701d7ae80264eeeed1bf0f237a5d0fdcbd48e8e4&"
+          src={logo}
           alt="Frog Meme Coin"
           className="w-64 h-64"
           data-aos="fade-down"
